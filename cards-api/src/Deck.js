329550b248eb22ba6aa60c639137b5e455b6e132
Dimitrios Tsarouchas <dimitristsaroucha@gmail.com>
2019-07-29 22:26:20 +0100

    Card API

---
+++ cards-api/src/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Deck extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+      </div>
+    )
+  }
+}
+export default Deck;
